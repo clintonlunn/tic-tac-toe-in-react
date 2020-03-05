12b497074a5bbfc5bc6a60758dfc1990890e3383
Clinton Lunn <cpl09c@gmail.com>
2020-03-04 22:25:13 -0500

    calculate winner

--- src/index.js
+++ src/index.js
@@ -25,6 +25,9 @@ class Board extends React.Component {
     const squares = this.state.squares.slice();
     console.log(squares);
 
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     (squares[i] = this.state.xIsNext) === true
       ? (squares[i] = "X")
       : (squares[i] = "O");
